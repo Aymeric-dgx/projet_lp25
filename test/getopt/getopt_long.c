1716b667b5682687afeafdf107013bd792d15f01
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-11-25 13:56:12 +0100

    Update getopt_long.c

--- test/getopt/getopt_long.c
+++ test/getopt/getopt_long.c
@@ -1,5 +1,10 @@
 #include <getopt.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/sysinfo.h>
+
+// ATTENTION : le faire sur linux, car sys/sysinfo.h n'existe pas sur windows
 
 /*
 NB : struct option est déja une strucrue définie dans le getopt.h
@@ -18,43 +23,51 @@ gcc -o main .\getopt_long.c ; .\main.exe --org --binary 25 --app hi
 
 
 int main(int argc, char *argv[]) {
-	int option = 0;
-	struct option my_opts[] = {
-		{.name="org",.has_arg=0,.flag=0,.val='o'},
-		{.name="app",.has_arg=1,.flag=0,.val='a'},
-		{.name="binary",.has_arg=2,.flag=0,.val='b'},
-		{.name=0,.has_arg=0,.flag=0,.val=0}, // last element must be zero
-	};
-	while((option = getopt_long(argc, argv, "oa:b::", my_opts, 0)) != -1) {
-        //printf("Puis option = %c optind = %d, optarg = %s\n", option, optind, optarg);
+    int verbose = 0;
+    char data_source[256] = "Undefined";
+    char temporary_directory[256] = "Undefined";
+    char output_file[256] = "Undefined";
+    int cpu_multiplicator = 1;
+    
+	
+	struct option options_list[] = {
+        {"verbose", 0, 0, 'v'},
+        {"data-source", 1, 0, 'd'},
+        {"temporary-directory", 1, 0, 't'},
+        {"output-file", 1, 0, 'o'},
+        {"cpu-multiplier", 1, 0, 'c'}
+    };
+    int option = 0;
+    while((option = getopt_long(argc, argv, "vd:t:o:c:", options_list, 0)) != -1) {
         switch(option) {
+            case 'v':
+                verbose = 1;
+                break;
+            
+            case 'd':
+                strcpy(data_source, optarg);
+                break;
+
+            case 't':
+                strcpy(data_source, optarg);
+                break;
+
             case 'o':
-                printf("-o active\n"); 
+                strcpy(data_source, optarg);
                 break;
-            case 'a':
-                printf("-a active with as argument %s\n", optarg);
-                break;
-            case 'b':
-                printf("-b active ");
-                // Cas possible : -b, -b25, -b 25 (avec potentiellement d'autre options après)
 
-                // Si l'argument est collé (ex : -b25), on fait comme pour -a
-                if(optarg != NULL) {
-                    printf("with %s\n", optarg);
-                }
-
-                // Sinon, on sait que le optind pointe l'argument (ou option) juste après le -b (car argument optionnel donc +1 et pas +2)
-                else {
-                    // Si on n'a pas atteint la fin ET que l'argument suivant n'est pas une option, alors cela veut dire que c'est l'argumenent de b
-                    if(optind < argc && argv[optind][0] != '-') { 
-                        printf("with %s\n", argv[optind]);
-                        optind++; // On fait optind++ afin de pointer sur l'option suivant (ex : -b 25 -a, ainsi on atteindra le -a)
-                    }
-                    // Sinon, b est activé sans argument
-                    else printf("without argument\n");
-                }
+            case 'c':
+                cpu_multiplicator = atoi(optarg);
                 break;
         }
-	}
+    }
+    char *str_verbose = (verbose == 1 ? "On" : "Off");
+
+    printf("Verbose mode is %s\n", str_verbose);
+    printf("Data source is located on %s\n", data_source);
+    printf("Temporary files will be located on %s\n", temporary_directory);
+    printf("Output file will be produces on  %s\n", output_file);
+    printf("%d tasks will run in parallel\n", cpu_multiplicator*get_nprocs());
+
 	return 0;
 }
