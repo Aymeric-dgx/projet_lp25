860e8a89521319886d309a2034b3f7593d2ac5ee
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-12-12 16:55:13 +0100

    Create header.h

---
+++ depot_aymeric/header.h
@@ -0,0 +1,23 @@
+#ifndef H_HEADER
+#define H_HEADER
+
+#include <pthread.h>
+#include <errno.h>
+
+// Nom des fichiers où seront enregistré les processus + fichier d'erreur
+extern const char main_file[];
+extern const char buffer_file[];
+extern const char errors_file[];
+
+// "Globalisation" des mutex afin que les differentes thread puissent bloquer par exemple être les seuls à modifer le main_file ou le errors_file
+extern pthread_mutex_t main_mutex;
+extern pthread_mutex_t error_mutex;
+
+
+// Déclaration des fonctions
+void local(const char* buffer_file, const char* main_file);
+void ui(const char* main_file);
+void write_error(const char error_msg[]);
+
+
+#endif
